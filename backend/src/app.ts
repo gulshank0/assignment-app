b8cdd55af9938839d2ee582a726c41542d65735d
gulshank0 <gulshanbahadur002@gmail.com>
2026-04-26 04:39:09 +0530

    final push

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -14,7 +14,7 @@ app.use(helmet());
 // ─── CORS ─────────────────────────────────────────────────────────────────────
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: process.env.CLIENT_URL || "https://assignment-app-v94h.vercel.app",
     credentials: true, // Allow cookies
     methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
